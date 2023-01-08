2ec0ef4aa1f17fb6f0f2a7027f4ec5f8caf1e054
Sean Harris <107904898+seanmharris@users.noreply.github.com>
2023-01-08 10:02:56 -0600

    Add files via upload

---
+++ 6kyu/BreakCamelCase.js
@@ -0,0 +1,35 @@
+/*
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+    string = string.replaceAll('A', ' A')
+    string = string.replaceAll('B', ' B')
+    string = string.replaceAll('C', ' C')
+    string = string.replaceAll('D', ' D')
+    string = string.replaceAll('E', ' E')
+    string = string.replaceAll('F', ' F')
+    string = string.replaceAll('G', ' G')
+    string = string.replaceAll('H', ' H')
+    string = string.replaceAll('I', ' I')
+    string = string.replaceAll('J', ' J')
+    string = string.replaceAll('K', ' K')
+    string = string.replaceAll('L', ' L')
+    string = string.replaceAll('M', ' M')
+    string = string.replaceAll('N', ' N')
+    string = string.replaceAll('O', ' O')
+    string = string.replaceAll('P', ' P')
+    string = string.replaceAll('Q', ' Q')
+    string = string.replaceAll('R', ' R')
+    string = string.replaceAll('S', ' S')
+    string = string.replaceAll('T', ' T')
+    string = string.replaceAll('U', ' U')
+    string = string.replaceAll('V', ' V')
+    string = string.replaceAll('W', ' W')
+    string = string.replaceAll('X', ' X')
+    string = string.replaceAll('Y', ' Y')
+    string = string.replaceAll('Z', ' Z')
+    return string
+  }
