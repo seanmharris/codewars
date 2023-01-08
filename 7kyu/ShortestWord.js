05562ff2b575513de44d3fb0afaf75fbafe7f9b7
Sean Harris <107904898+seanmharris@users.noreply.github.com>
2023-01-08 10:02:39 -0600

    Add files via upload

---
+++ 7kyu/ShortestWord.js
@@ -0,0 +1,7 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+const findShort = s => Math.min(...s.split(' ').map(item => item.length))
