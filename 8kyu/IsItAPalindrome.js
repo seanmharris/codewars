daf0ba3b1906fd2b844d425066e0415d97dfda20
Sean Harris <107904898+seanmharris@users.noreply.github.com>
2023-01-11 16:33:25 -0600

    Add files via upload

---
+++ 8kyu/IsItAPalindrome.js
@@ -0,0 +1,5 @@
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+*/
+
+const isPalindrome = x => x.toLowerCase() == x.split('').reverse().join('').toLowerCase()
