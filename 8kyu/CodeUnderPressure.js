cf311be948f1c697dc612ce9be7f76bef3954d5e
Sean Harris <107904898+seanmharris@users.noreply.github.com>
2023-01-24 12:33:48 -0600

    Add files via upload

---
+++ 8kyu/CodeUnderPressure.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+}
+
+// everything but the *2 was already there. So this took me approx 3 seconds, most of which was moving my mouse to put the caret in the right spot
+
