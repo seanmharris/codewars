25f0c89013814f9dc1e7bd268aff87997fde1862
Sean Harris <107904898+seanmharris@users.noreply.github.com>
2023-01-25 10:49:53 -0600

    Add files via upload

---
+++ 8kyu/FindThePosition.js
@@ -0,0 +1,9 @@
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+*/
+
+const position = letter => `Position of alphabet: ${letter.charCodeAt(0) - 96}`
