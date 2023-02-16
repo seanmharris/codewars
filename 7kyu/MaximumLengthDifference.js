167539c43094d2d41a1afd5ee90b4b446f26a220
Sean Harris <seanmharris92@gmail.com>
2023-02-16 08:52:29 -0600

    first kata Feb 16

---
+++ 7kyu/MaximumLengthDifference.js
@@ -0,0 +1,23 @@
+/*
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string
+*/
+
+function mxdiflg(a1, a2) {
+    if(!a1.length || !a2.length) return -1
+    const mapLen = arr => arr.map(str => str.length)
+    let lengths = [mapLen(a1), mapLen(a2)]
+    const distances = [Math.max(...lengths[0]) - Math.min(...lengths[1]), Math.min(...lengths[0])-Math.max(...lengths[1])]
+    return Math.max(Math.abs(distances[0]), Math.abs(distances[1]))
+}  
