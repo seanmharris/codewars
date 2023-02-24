b4daedb748c9009e16b45bb7ef6154e10ab70022
Sean Harris <seanmharris92@gmail.com>
2023-02-24 07:57:40 -0600

    first kata Feb 24

---
+++ 8kyu/AreArrowFunctionsOdd.js
@@ -0,0 +1,7 @@
+/*
+Time to test your basic knowledge in functions! Return the odds from a list:
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+*/
+
+const odds = values => values.filter(num => num % 2 !== 0)
