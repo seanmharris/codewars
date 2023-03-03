717d2582f8bd8e682be99403b1e0d392970a94e7
Sean Harris <seanmharris92@gmail.com>
2023-03-03 12:13:10 -0600

    third kata Mar 3

---
+++ 8kyu/GrasshopperVariableAssignmentDebug.js
@@ -0,0 +1,16 @@
+/*
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+Given code:
+var a == "dev"
+var b == "Lab"
+
+var name == a + b
+
+Solved code:
+*/
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
