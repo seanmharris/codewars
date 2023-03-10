bfcaa652888e6a18795e3bf03f5ea32fa663e3a0
Sean Harris <seanmharris92@gmail.com>
2023-03-10 08:30:22 -0600

    first kata Mar 10

---
+++ 7kyu/Switcheroo.js
@@ -0,0 +1,13 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa).
+Leave any incidence of c untouched.
+Example:
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x){
+    let step1 = x.replaceAll('b','d')
+    let step2 = step1.replaceAll('a','b')
+    return step2.replaceAll('d','a')
+  }
