471f7fc158634aa489fa41825e453f1c91dba062
Sean Harris <seanmharris92@gmail.com>
2023-03-13 09:08:59 -0500

    third kata Mar 13

---
+++ 8kyu/ExclamationMarksSeries2RemoveAllExlamationMarksFromTheEndOfSentence.js
@@ -0,0 +1,12 @@
+/*
+Remove all exclamation marks from the end of sentence.
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+const remove = string => string.endsWith('!') ? remove(string.slice(0, string.length - 1)) : string
