88411879d11c99cccd00a8eb09dffc58a87735ba
Sean Harris <seanmharris92@gmail.com>
2023-03-19 08:37:01 -0500

    fourth kata Mar 19

---
+++ 7kyu/SimpleRemoveDuplicates.js
@@ -0,0 +1,18 @@
+/*
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+*/
+
+function solve(arr) {
+  let ans = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if(ans.includes(arr[i])) continue;
+    ans.unshift(arr[i])
+  }
+  return ans
+}
