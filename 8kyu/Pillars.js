ae5e1b802bafea4a10955ad93212e7718725c913
Sean Harris <seanmharris92@gmail.com>
2023-03-20 08:44:34 -0500

    first kata Mar 20

---
+++ 8kyu/Pillars.js
@@ -0,0 +1,9 @@
+/*
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+*/
+
+const pillars = (numPill, dist, width) => (numPill - 1) * dist * 100 + (numPill > 1 ? numPill - 2 : 0) * width
