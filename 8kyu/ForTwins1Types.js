e8d9f108300c5823295e7725df79e7fffa593859
Sean Harris <seanmharris92@gmail.com>
2023-04-14 09:02:27 -0500

    second kata Apr 14

---
+++ 8kyu/ForTwins1Types.js
@@ -0,0 +1,9 @@
+/*
+Write a function that will accept two parameters: variable and type and check 
+if type of variable is matching type. Return true if types match or false if not.
+Examples:
+42, "number"   --> true
+"42", "number" --> false
+*/
+
+const typeValidation = (v,t) => typeof v === t
