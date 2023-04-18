799770aadf6d9d66a55c04dcba7439f3bfe8c348
Sean Harris <seanmharris92@gmail.com>
2023-04-18 09:00:42 -0500

    second kata Apr 18

---
+++ 7kyu/HowManyArguments.js
@@ -0,0 +1,10 @@
+/*
+Create a function args_count() 
+It should simply return the number of arguments passed into it
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+function args_count() {
+    return arguments.length
+  }
