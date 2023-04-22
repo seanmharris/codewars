d4fe9f154db8e4592b54d8725f1b83240811aa7f
Sean Harris <seanmharris92@gmail.com>
2023-04-22 10:40:42 -0500

    third kata Apr 22

---
+++ 8kyu/GrasshopperObjectSyntaxDebug.js
@@ -0,0 +1,42 @@
+/*
+Object debugging
+While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working.
+Find all of the errors in the rooms object to get your game working again.
+*/
+
+/* given code to debug:
+var rooms = {
+    first: {
+      description: 'This is the first room'
+      items: {
+        chair: 'The old chair looks comfortable',
+        lamp: 'This lamp looks ancient'
+    },
+    second: {
+      description: 'This is the second room'
+      items: {
+        couch: 'This couch looks like it would hurt your back,
+        table: 'On the table there is an unopened bottle of water'
+      }
+    }
+  }
+*/
+
+// my solution (added a few commas, a single quote, and an ending bracket):
+
+var rooms = {
+    first: {
+      description: 'This is the first room',
+      items: {
+        chair: 'The old chair looks comfortable',
+        lamp: 'This lamp looks ancient'
+    },
+    second: {
+      description: 'This is the second room',
+      items: {
+        couch: 'This couch looks like it would hurt your back',
+        table: 'On the table there is an unopened bottle of water'
+      }
+    }
+    }
+  }
