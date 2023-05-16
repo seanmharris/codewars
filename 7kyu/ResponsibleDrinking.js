3ba24981534dd6c162021d4814f0becefceac5aa
Sean Harris <seanmharris92@gmail.com>
2023-05-16 07:45:10 -0500

    second kata May 16

---
+++ 7kyu/ResponsibleDrinking.js
@@ -0,0 +1,16 @@
+/*
+Welcome to the Codewars Bar!
+Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+Examples
+"1 beer"  -->  "1 glass of water"
+because you drank one standard drink
+"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+because you drank ten standard drinks
+Note: to keep things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...
+*/
+
+function hydrate(s) {
+    let num = s.split(' ').filter(el => +el >= 1).reduce((a,b) => a + +b, 0)
+    return `${num} glass${num > 1 ? 'es' : ''} of water`
+  }
