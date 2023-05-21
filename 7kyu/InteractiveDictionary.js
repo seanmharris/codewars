092cb5e10d2cf2ca0ef583a0d37bebd4d375312c
Sean Harris <seanmharris92@gmail.com>
2023-05-21 10:55:24 -0500

    fourth kata May 21

---
+++ 7kyu/InteractiveDictionary.js
@@ -0,0 +1,21 @@
+/*
+In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+>>> let d = new Dictionary();
+>>> d.newEntry("Apple", "A fruit that grows on trees");
+>>> console.log(d.look("Apple"));
+A fruit that grows on trees
+>>> console.log(d.look("Banana"));
+Can't find entry for Banana
+Good luck and happy coding!
+*/
+
+class Dictionary {
+    constructor() {
+      this.newEntry =  function(key,value) {
+        this[key] = value;
+      },
+      this.look = function(key) {
+        return this[key] ? this[key] : `Can't find entry for ${key}`
+      }
+    }
+  }
