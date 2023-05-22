1e375221ae81931a9fc1614c49ac5aa9a2d360cf
Sean Harris <seanmharris92@gmail.com>
2023-05-22 11:30:40 -0500

    second kata May 22

---
+++ 8kyu/GeometryBasicsCircleAreaIn2D.js
@@ -0,0 +1,23 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+The Circle class can be seen below:
+// Represents a Circle where center is a Point and radius is a Number
+class Circle {
+  constructor(center, radius) { 
+    this.center = center; 
+    this.radius = radius;
+  }
+}
+And the Point class can be seen below:
+// Represents a Point where x and y are Numbers
+class Point {
+  constructor (x,y) { 
+    this.x = x;
+    this.y = y; 
+  }
+}
+Tests round answers to 6 decimal places.
+*/
+
+const circleArea = circle => +(circle.radius ** 2 * Math.PI).toFixed(6)
