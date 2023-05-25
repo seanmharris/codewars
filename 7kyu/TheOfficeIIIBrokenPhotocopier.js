a224749877782d90b62eb0e1393742bc4b8e1e73
Sean Harris <seanmharris92@gmail.com>
2023-05-25 10:01:34 -0500

    first kata May 25

---
+++ 7kyu/TheOfficeIIIBrokenPhotocopier.js
@@ -0,0 +1,15 @@
+/*
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+Given a string of binary, return the version the photocopier gives you as a string.
+*/
+
+function broken(x){
+    let res = ''
+    for (let char of x) {
+      if (char === '1') {
+        res += '0'
+      } else res += '1'
+    }
+    return res
+  }
