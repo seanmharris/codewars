855c1c4eee085fefd9040c9b28e1620cf460913c
Sean Harris <seanmharris92@gmail.com>
2023-05-28 12:49:12 -0500

    second kata May 28

---
+++ 8kyu/GeometryBasicsCircleCircumferenceIn2D.js
@@ -0,0 +1,9 @@
+/*
+Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+Write a function calculating circumference of a Circle.
+Tests round answers to 6 decimal places.
+*/
+
+function circleCircumference(circle) {
+    return +(circle.radius * 2 * Math.PI).toFixed(6)
+  }
