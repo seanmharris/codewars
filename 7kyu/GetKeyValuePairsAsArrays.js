69eeb28e89ad5f4160d0d07bd0d68f3064c86918
Sean Harris <seanmharris92@gmail.com>
2023-06-14 07:30:35 -0500

    first kata Jun 14

---
+++ 7kyu/GetKeyValuePairsAsArrays.js
@@ -0,0 +1,7 @@
+/*
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+Example:
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+*/
+
+const keysAndValues = data => [Object.keys(data), Object.values(data)]
