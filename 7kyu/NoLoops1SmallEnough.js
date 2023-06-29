fc1f5f460cbd1225cbf7217b1cc113f0e4689b77
Sean Harris <seanmharris92@gmail.com>
2023-06-29 10:19:42 -0500

    first kata Jun 29

---
+++ 7kyu/NoLoops1SmallEnough.js
@@ -0,0 +1,10 @@
+/*
+*** No Loops Allowed ***
+You will be given an array (a) and a limit value (limit).
+You must check that all values in the array are below or equal to the limit value.
+If they are, return true. Else, return false.
+You can assume all values in the array are numbers.
+Do not use loops. Do not modify input array.
+*/
+
+const smallEnough = (a, limit) => a.every(x => x <= limit)
