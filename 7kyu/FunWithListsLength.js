860034c6426adc3ff9782ae6063b860a806131bb
Sean Harris <seanmharris92@gmail.com>
2023-07-16 10:06:10 -0500

    second kata Jul 16

---
+++ 7kyu/FunWithListsLength.js
@@ -0,0 +1,12 @@
+/*
+Implement the method length, which accepts a linked list (head), and returns the length of the list.
+For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+The linked list is defined as follows:
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+Note: the list may be null and can hold any type of value.
+*/
+
+const length = head => head ? 1 + length(head.next) : 0
