ab173136daebbbe7cd31b5fb9a1612836315ad8b
Sean Harris <seanmharris92@gmail.com>
2023-07-24 08:38:24 -0500

    first kata Jul 24

---
+++ 7kyu/Recursion101.js
@@ -0,0 +1,17 @@
+/*
+In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+a and b will both be lower than 10E8.
+*/
+
+function solve(a,b) {
+    if (!a || !b) return [a,b]
+    if (a >= 2 * b) return solve(a - 2 * b, b)
+    if (b >= 2 * a) return solve(a, b - 2 * a)
+    return [a,b]
+  }
+
+// one line but pretty tough to read
+const oneLineSolve = (a,b) => (!a || !b) ? [a,b] : a >= 2 * b ? oneLineSolve(a - 2 * b, b) : b >= 2 * a ? oneLineSolve(a, b - 2 * a)  : [a,b]
