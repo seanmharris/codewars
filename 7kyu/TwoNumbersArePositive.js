ad7bd2c8eb808b26e9f70e5e517ce93ad642dec3
Sean Harris <seanmharris92@gmail.com>
2023-08-04 09:54:29 -0500

    first kata Aug 4

---
+++ 7kyu/TwoNumbersArePositive.js
@@ -0,0 +1,14 @@
+/*
+Task:
+Your job is to write a function, which takes three integers a, b, and c as arguments,
+and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+Examples:
+twoArePositive(2, 4, -3) == true
+twoArePositive(-4, 6, 8) == true
+twoArePositive(4, -6, 9) == true
+twoArePositive(-4, 6, 0) == false
+twoArePositive(4, 6, 10) == false
+twoArePositive(-14, -3, -4) == false
+*/
+
+const twoArePositive = (a, b, c) => (a > 0 && b > 0 && c <= 0) || (a > 0 && c > 0 && b <= 0) || (b > 0 && c > 0 && a <= 0)
