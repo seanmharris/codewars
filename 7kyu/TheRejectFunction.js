bc5666c5beba25b77f993e5ab375bfa75b80725e
Sean Harris <seanmharris92@gmail.com>
2023-08-13 09:20:33 -0500

    second kata Aug 13

---
+++ 7kyu/TheRejectFunction.js
@@ -0,0 +1,6 @@
+/*
+Implement a function which filters out array values which satisfy the given predicate.
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
+*/
+
+const reject = (array, predicate) => array.filter(x => !predicate(x))
