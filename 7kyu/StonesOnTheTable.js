ec5a34e41ef064ac5c749e30dc6687766c811b6b
Sean Harris <seanmharris92@gmail.com>
2023-08-13 09:17:49 -0500

    first kata Aug 13

---
+++ 7kyu/StonesOnTheTable.js
@@ -0,0 +1,16 @@
+/*
+There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
+Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colors.
+Examples:
+"RGBRGBRGGB"   => 1
+"RGGRGBBRGRR"  => 3
+"RRRRGGGGBBBB" => 9
+*/
+
+function solve(stones) {
+    let x = 0
+    for (let i = 1; i < stones.length; i++) {
+      if (stones[i] === stones[i - 1]) x++
+    }
+    return x
+  }
