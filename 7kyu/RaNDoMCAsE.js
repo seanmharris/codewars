c375a9c475f74502128b86ea14f424494c1ef858
Sean Harris <seanmharris92@gmail.com>
2023-08-15 08:03:05 -0500

    second kata Aug 15

---
+++ 7kyu/RaNDoMCAsE.js
@@ -0,0 +1,18 @@
+/*
+Write a function that will randomly upper and lower characters in a string - randomCase() (random_case() for Python).
+A few examples:
+randomCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit") == "lOReM ipSum DOloR SiT AmeT, cOnsEcTEtuR aDiPiSciNG eLIt"
+randomCase("Donec eleifend cursus lobortis") == "DONeC ElEifEnD CuRsuS LoBoRTIs"
+Notes:
+This function will work within the basic ASCII character set to make this kata easier - so no need to make the function multibyte safe.
+The letters MUST be selected randomly - filters are set to make sure there is no cheating!
+*/
+
+function randomCase(x) {
+    let res = ''
+    for (let char of x) {
+      let choose = Math.random() * 100
+      res += (choose > 50 ? char.toUpperCase() : char.toLowerCase())
+    }
+    return res
+  }
