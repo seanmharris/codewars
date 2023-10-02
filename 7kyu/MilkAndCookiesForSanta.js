ff66eac257f3101c5aa243576d3bc21da06977e5
Sean Harris <seanmharris92@gmail.com>
2023-10-02 12:02:59 -0500

    second kata Oct 2

---
+++ 7kyu/MilkAndCookiesForSanta.js
@@ -0,0 +1,7 @@
+/*
+It's almost Christmas Eve, so we need to prepare some milk and cookies for Santa! Wait... when exactly do we need to do that?
+Time for Milk and Cookies
+Complete the function function that accepts a Date object, and returns true if it's Christmas Eve (December 24th), false otherwise.
+*/
+
+const timeForMilkAndCookies = date => date.getMonth() === 11 && date.getDate() === 24
